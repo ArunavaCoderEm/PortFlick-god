2ce5d5a6490ecd8b83b804be2fce4ca1a7dbb950
ArunavaCoderEm <acecankill@gmail.com>
2025-02-02 23:07:11 +0530

    Features section done, only text changes remains

--- src/Sections/features-section.jsx
+++ src/Sections/features-section.jsx
@@ -1,38 +1,45 @@
-import { Activity, AlignCenterHorizontalIcon, AppWindow, User } from "lucide-react";
+import {
+  Activity,
+  AlignCenterHorizontalIcon,
+  AppWindow,
+  User,
+} from "lucide-react";
 import React from "react";
 import { motion } from "framer-motion";
 
 const Features = () => {
-
-    const mapfeature =  [
-          {
-            title: "Seamless Authentication",
-            description: "Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.",
-            icon: <User />
-          },
-          {
-            title: "Portfolio Templates",
-            description: "Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.",
-            icon: <AppWindow />
-          },
-          {
-            title: "Modern Design",
-            description: "Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.",
-            icon: <AlignCenterHorizontalIcon />
-          },
-          {
-            title: "Modern Design",
-            description: "Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.",
-            icon: <AlignCenterHorizontalIcon />
-          },
-          {
-            title: "Modern Design",
-            description: "Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.",
-            icon: <AlignCenterHorizontalIcon />
-          },
-        ]
-      
-      
+  const mapfeature = [
+    {
+      title: "Seamless Authentication",
+      description:
+        "Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.",
+      icon: <User className="w-10 h-10 fill-purple-500 rounded-full text-white" />,
+    },
+    {
+      title: "Portfolio Templates",
+      description:
+        "Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.",
+      icon: <AppWindow className="w-10 h-10 fill-purple-500 rounded-full text-white" />,
+    },
+    {
+      title: "Modern Design",
+      description:
+        "Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.",
+      icon: <AlignCenterHorizontalIcon className="w-10 h-10 fill-purple-500 rounded-full text-white" />,
+    },
+    {
+      title: "Modern Design",
+      description:
+        "Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.",
+      icon: <AlignCenterHorizontalIcon className="w-10 h-10 fill-purple-500 rounded-full text-white" />,
+    },
+    {
+      title: "Modern Design",
+      description:
+        "Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.",
+      icon: <AlignCenterHorizontalIcon className="w-10 h-10 fill-purple-500 rounded-full text-white" />,
+    },
+  ];
 
   return (
     <div className="barlow px-2 py-1">
@@ -47,18 +54,43 @@ const Features = () => {
         <Activity size={24} className="text-white" />
       </motion.h1>
 
-        <div className="text-white">
-            {mapfeature.slice(0, 3).map((item, index) => {
-                return (
-                    <div key={index}>
-                        {item.title}
-                        {item.description}
-                        {item.icon}
-                    </div>
-                )
-            })}
-        </div>
+      <div className="text-white gap-4 grid md:grid-cols-1 grid-cols-1 lg:grid-cols-3 p-3">
+        {mapfeature.slice(0, 3).map((item, index) => {
+          return (
+            <div key={index} className="rounded-lg px-4 py-3 bg-gradient-to-b from-pink-700 shadow shadow-purple-900 to-pink-900">
+              <span className="mb-1 rounded-full">
+                {item.icon}
+              </span>
+              <div>
+                <h1 className="text-white mt-5 font-semibold text-xl md:text-2xl">
+                  {item.title}
+                </h1>
+                <p className="text-white font-normal text-md md:text-lg mt-2">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
 
+      <div className="text-white gap-4 flex md:flex-row flex-col justify-center p-3">
+        {mapfeature.slice(3, 5).map((item, index) => {
+          return (
+            <div key={index} className="rounded-lg md:max-w-[50vw] w-full lg:max-w-[33.3vw] px-4 py-3 bg-gradient-to-b from-pink-700 shadow shadow-purple-900 to-pink-900">
+              <span className="mb-1 rounded-full">{item.icon}</span>
+              <div>
+                <h1 className="text-white mt-5 font-semibold text-xl md:text-2xl">
+                  {item.title}
+                </h1>
+                <p className="text-white font-normal text-md md:text-lg mt-2">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
