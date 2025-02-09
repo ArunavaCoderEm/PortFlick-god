7464b8e1ee45d217e3d48aff12c5828e250cc559
ArunavaCoderEm <acecankill@gmail.com>
2025-02-09 22:52:52 +0530

    minor fixes now will write useFetch with call back and implement tanstack

--- src/components/my-component/app-sidebar.tsx
+++ src/components/my-component/app-sidebar.tsx
@@ -44,7 +44,7 @@ export function AppSidebar():React.ReactNode {
               <SidebarInput
                 id="search"
                 placeholder="Search dashboard..."
-                className="pl-8"
+                className="pl-8 ring-purple-500 border-none focus:border-none focus:outline-none outline-none focus:ring focus:ring-purple-500"
               />
               <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
             </SidebarGroupContent>
